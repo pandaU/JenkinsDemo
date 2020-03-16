4fe319d09b611d438639272651718b7d1db89422
谢雄雄 <xiexiongxiong@talkweb.com.cn>
2020-03-16 15:46:12 +0800

    first  commit

---
+++ src/main/java/com/example/cicd/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.cicd.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @RequestMapping("/")
+    public Object  login(){
+        return "hello Jenkins";
+    }
+}
